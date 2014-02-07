ebb353002e27a7e1153f428242410bb60dd71758
vikingsheepman <vikingsheepman@gmail.com>
2014-02-06 23:21:11 -0700

    parser.scm
    start of parser program

---
+++ parser.scm
@@ -0,0 +1,320 @@
+#!/usr/bin/env guile
+!#
+
+
+;; Declare Non-terminal parser stubs
+(define (system-goal)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (program)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (program-heading)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (block)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (variable-declaration-part)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (variable-declaration-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (variable-declaration)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (type)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (procedure-and-function-declaration-part)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (procedure-declaration)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (function-declaration)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (procedure-heading)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (function-heading)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (optional-formal-paramater-list)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (formal-parameter-section-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (formal-parameter-section)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (value-parameter-section)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (variable-parameter-section)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (statement-part)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (compound-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (statement-sequence)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (statement-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (empty-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (read-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (read-parameter-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (read-parameter)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (write-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (write-parameter-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (write-parameter)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (assignment-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (if-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (optional-else-part)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (repeat-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (while-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (for-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (control-variable)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (initial-value)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (step-value)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (final-value)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (procedure-statement)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (optional-actual-parameter-list)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (actual-parameter-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (actual-parameter)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (expression)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (optional-relation-part)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (relational-operator)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (simple-expression)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (term-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (optional-sign)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (adding-operator)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (term)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (factor-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (multiplying-operator)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (factor)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (program-identifier)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (variable-identifier)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (procedure-identifier)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (function-identifier)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (boolean-expression)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (ordinal-expression)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (identifier-list)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
+(define (identifier-tail)
+  (let ((next-token (get-token)))
+  (cond (() )
+        (() ))))
+
